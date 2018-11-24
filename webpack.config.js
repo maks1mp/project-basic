b16e11d833c2d82046026a736355a2dcb092ba08
Student <student@Redmi4-GLaDOSPhone.localdomain>
2018-11-24 11:34:46 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+require('babel-register');
+
+const config = {
+  mode: 'development',
+  entry: './script/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'script', 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules : [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      hash: true
+    })
+  ]
+};
+
+module.exports = config;
